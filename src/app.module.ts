a5c03ac5d8cf0191b3c4f3ef76db0c86a2f3dd8a
Elarbi Allam <elarbi.allam.51@gmail.com>
2025-06-02 02:01:46 +0100

    feat: début implémentation module tasks

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ProjectsModule } from './projects/projects.module';
-import { TasksModule } from './tasks/tasks.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
