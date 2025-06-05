bfdf4878bf5786a260406172d0a09cb83b9eba25
dahbimoad <dahbimoad1@gmail.com>
2025-06-05 16:25:39 +0100

    implement pagination for GET /projects endpoint

--- src/projects/dto/index.ts
+++ src/projects/dto/index.ts
@@ -2,3 +2,9 @@ export { CreateProjectDto } from './create-project.dto';
 export { UpdateProjectDto } from './update-project.dto';
 export { InviteUserDto } from './invite-user.dto';
 export { ProjectResponseDto, InviteResponseDto } from './project-response.dto';
+export { 
+  PaginationQueryDto, 
+  PaginationMetaDto, 
+  PaginatedResponseDto 
+} from './pagination.dto';
+export { PaginatedProjectsResponseDto } from './paginated-projects-response.dto';
